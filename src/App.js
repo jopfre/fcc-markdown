9c1cf99169cdc3de926245e2caa146e39db934b0
jopfre <jonah.freeland@gmail.com>
2019-02-01 18:49:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 
-import Posts from './components/Posts';
-import PostForm from './components/Postform';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
 
 import store from './store';
 
 class App extends Component {
+  componentDidMount() {
+    const fcc = document.createElement("script");
+    fcc.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    document.body.appendChild(fcc);
+  }
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <PostForm />
-          <hr />
-          <Posts />
+          <h1 className="App-title">Markdown Previewer</h1>
+          <div className="row">
+            <Editor />
+            <Preview />
+          </div>
         </div>
       </Provider>
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import postReducer from './postReducer';
+import markdownReducer from './markdownReducer';
 
 export default combineReducers({
-  posts: postReducer
+  data: markdownReducer
 });
